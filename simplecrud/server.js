5a018071f857ce891a88e1cbea34625def4c6deb
Eric Tung <eric.p.tung@gmail.com>
2017-12-05 18:08:01 -0800

    Figure out reason why collection didn't work

--- simplecrud/server.js
+++ simplecrud/server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(3000, function() {
-  console.log('listening on 3000');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const url = 'mongodb://admin:admin@ds131826.mlab.com:31826/mongo_playground';
+let db;
+
+MongoClient.connect(url, (err, database) => {
+  if (err) return console.log(err);
+  db = database;
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  });
 });
+
+// app.listen(3000, function() {
+//   console.log('listening on 3000');
+// });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if(err) return console.log(err)
+    console.log('saved to database');
+    res.redirect('/');
+  });
 });
