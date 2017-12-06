741ab8ed5bde04836c0ac2c45a8a629061102e9f
Eric Tung <eric.p.tung@gmail.com>
2017-12-06 12:25:52 -0800

    Generate react components

--- mern-comment-box/src/index.js
+++ mern-comment-box/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from ‘react’;
+import ReactDOM from ‘react - dom’;
+import CommentBox from ‘./CommentBox’;
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById(‘root’)
+);
+
 registerServiceWorker();
