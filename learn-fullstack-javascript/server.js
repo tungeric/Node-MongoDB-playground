105f67ad91a1f86e2a3975d172bc62b497e7caab
Eric Tung <eric.p.tung@gmail.com>
2017-12-07 01:02:00 -0800

    Include ejs and create public and views

--- learn-fullstack-javascript/server.js
+++ learn-fullstack-javascript/server.js
@@ -1,8 +1,9 @@
+// // 1. 
 // import config, { nodeEnv } from './config';
 // console.log(config);
 // console.log(nodeEnv);
 
-// // You can use http to directly ping a url and return data
+// // 2. You can use http to directly ping a url and return data
 // // now run `babel-node server.js` in terminal
 // import https from 'https';
 // https.get('https://www.cnn.com/', res => {
@@ -13,7 +14,7 @@
 //   });
 // });
 
-// // Create a server with pure http and use it to do basic things in terminal
+// // 3. Create a server with pure http and use it to do basic things in terminal
 // import http from 'http';
 // const server = http.createServer();
 // server.listen(8080);
@@ -28,18 +29,28 @@
 // // now run `npm start` which runs `babel-node server.js`
 // // then in another terminal window run `curl http://localhost:8080/`
 
-// Use express!
+// 4. Use express!
 import config from './config';
+// import fs from 'fs';
+import apiRouter from './api';
 import express from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.render('index', {
+    content: 'Trust the Process'
+  });
 });
 
-server.get('/about.html', (req, res) => {
-  res.send('The about page');
-});
+server.set('view engine', 'ejs');
+server.use('/api', apiRouter);
+server.use(express.static('public')); //does the equivalent of the code below!
+// server.get('/about.html', (req, res) => {
+//   // res.send('The about page');
+//   fs.readFile('./about.html', (err, data) => {
+//     res.send(data.toString());
+//   });
+// });
 
 server.listen(config.port, () => {
   console.info('Express listening on port', config.port);
